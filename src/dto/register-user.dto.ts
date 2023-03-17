1f1364a0a8211fe06c892b7aa7108cbe742cc34f
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2023-03-17 15:59:46 -0300

    migrating DTOs to Zod

--- src/dto/register-user.dto.ts
+++ src/dto/register-user.dto.ts
@@ -1,5 +1,22 @@
-export interface IRegisterUserDTO {
-  name: string;
-  email: string;
-  password: string;
-}
+import { z } from 'zod';
+
+export const RegisterUserSchema = z.object({
+  name: z.string({
+    required_error: 'Name is required',
+    invalid_type_error: 'Must be a string',
+  }),
+  email: z
+    .string({
+      required_error: 'Email is required',
+      invalid_type_error: 'Must be a string',
+    })
+    .email('Wrong format for an email'),
+  password: z
+    .string({
+      required_error: 'Password is required',
+      invalid_type_error: 'Must be a string',
+    })
+    .min(6, 'Password must contain at least 6 characters'),
+});
+
+export type RegisterUserDTO = z.infer<typeof RegisterUserSchema>;
--- src/dto/update-password.dto.ts
+++ src/dto/update-password.dto.ts
@@ -1,4 +1,8 @@
-export interface UpdatePasswordDTO {
-  email: string;
-  password: string;
-}
+import { z } from 'zod';
+
+export const UpdatePasswordSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+export type UpdatePasswordDTO = z.infer<typeof UpdatePasswordSchema>;
